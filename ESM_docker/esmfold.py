48d31889c2b25c2dff440e1d2ab615f9db253964
Matthew Nemeth <matthewn@arcinstitute.org>
2023-06-04 11:59:30 -0700

    create docker for esmfold job

---
+++ ESM_docker/esmfold.py
@@ -0,0 +1,101 @@
+from transformers import AutoTokenizer, EsmForProteinFolding
+from accelerate import cpu_offload
+from transformers.models.esm.openfold_utils.protein import to_pdb, Protein as OFProtein
+from transformers.models.esm.openfold_utils.feats import atom14_to_atom37
+from Bio import SeqIO
+import torch
+from time import time
+import sys
+import subprocess
+import os
+import numpy as np
+
+def download_from_bucket(bucket_name):
+    """
+    Downloads a blob from the bucket to a local file named input.fasta
+    """
+    try:
+        process = subprocess.run(['gsutil', 'cp', f'gs://{bucket_name}/input.fasta', "input.fasta"], check=True)
+    except subprocess.CalledProcessError as e:
+        print(f'Error occurred while downloading file from GCS: {e}')
+
+def write_to_bucket(bucket_name, file_name):
+    try:
+        process = subprocess.run(['gsutil', 'cp', file_name, f'gs://{bucket_name}/{file_name}'], check=True)
+    except subprocess.CalledProcessError as e:
+        print(f'Error occurred while downloading file from GCS: {e}')
+        
+        
+def process_fasta(file):
+    tokenizer = AutoTokenizer.from_pretrained("facebook/esmfold_v1")
+    records = list(SeqIO.parse(file, "fasta"))
+    sequence = str(records[0].seq)
+    
+    tokenized_input = tokenizer([sequence], return_tensors="pt", add_special_tokens=False)['input_ids']
+    tokenized_input = tokenized_input.to(device)
+    return tokenized_input
+        
+def convert_outputs_to_pdb(outputs):
+    final_atom_positions = atom14_to_atom37(outputs["positions"][-1], outputs)
+    outputs = {k: v.to("cpu").numpy() for k, v in outputs.items()}
+    final_atom_positions = final_atom_positions.cpu().numpy()
+    final_atom_mask = outputs["atom37_atom_exists"]
+    pdbs = []
+    for i in range(outputs["aatype"].shape[0]):
+        aa = outputs["aatype"][i]
+        pred_pos = final_atom_positions[i]
+        mask = final_atom_mask[i]
+        resid = outputs["residue_index"][i] + 1
+        pred = OFProtein(
+            aatype=aa,
+            atom_positions=pred_pos,
+            atom_mask=mask,
+            residue_index=resid,
+            b_factors=outputs["plddt"][i],
+            chain_index=outputs["chain_index"][i] if "chain_index" in outputs else None,
+        )
+        pdbs.append(to_pdb(pred))
+    return pdbs
+
+def esmfold(tokenized_input):
+    # Loading and prepping model
+    model = EsmForProteinFolding.from_pretrained("facebook/esmfold_v1", low_cpu_mem_usage=True)
+    model = model.cuda()
+    
+    # Speedups and optim
+    # Uncomment to switch the stem to float16
+    model.esm = model.esm.half()
+    torch.backends.cuda.matmul.allow_tf32 = True
+    # Uncomment this line if your GPU memory is 16GB or less, or if you're folding longer (over 600 or so) sequences
+    model.trunk.set_chunk_size(64)
+    cpu_offload(model)
+    
+    # Run and benchmark time for esmfold
+    start_time = time()
+    with torch.no_grad():
+        output = model(tokenized_input, num_recycles=10)
+    process_time = time() - start_time
+    print(f"Took {process_time} seconds")
+    return output
+    
+def save_output(output):
+    pdb = convert_outputs_to_pdb(output)
+    with open("output.pdb", "w") as f:
+        f.write("".join(pdb))
+    
+if __name__ == "__main__":
+    bucket = sys.argv[1]
+    print(f"I got the following bucket path: {bucket}")
+    print("Attempting to pull from the bucket...")
+    download_from_bucket(bucket)
+    print(f"{os.listdir('.')}")
+    
+    device = torch.cuda.current_device()
+    print("Tokenizing the input")
+    tokenized_input = process_fasta("input.fasta")
+    print("Running ESMFold.")
+    output = esmfold(tokenized_input)
+    print("Saving outputs to pdb")
+    save_output(output)
+    print("Copying outputs to bucket")
+    write_to_bucket(bucket, "output.pdb")
--- README.md
+++ README.md
@@ -9,16 +9,48 @@ Use cases:
 
 
 Deployment:
-1. Alphafold docker container which is called every time you want to generate a prediction.
-    This is too big to do locally.
-2. 
+
+```mermaid
+graph TB
+  F["Frontend (Deployment)"] -- "Job Request" --> KM["Kubernetes Manager"]
+  KM -- "Create Job" --> K["Kubernetes Cluster"]
+  K -- "Run Job (Pod with ML script)" --> P["Pod with T4 GPU"]
+  F -- "Input File" --> P
+  P -- "Run ML Inference" --> R["Results"]
+  R -- "Return Results" --> F
+```
+1. Frontend: Kubernetes Deployment
+2. Backend (Kubernetes Manager): Google Cloud Functions
+3. ML Inference: Kubernetes Job
+4. File communication: Google Cloud Buckets
 
 
-Progress
+### Progress
+Structure
+- [x] Architecture Diagram
+
+Frontend
 - [x] frontend input
-- [] test running ESM on GCP instance
-- [] packaging ESM into a dockerfile or Google Cloud Function
-- [] call ESM from frontend
+- [] Add File icon to input box once FASTA file has been inputted
 - [] mock output for frontend
 - [] probability distribution viz, protein 3d viz for frontend
-- [] (optional) ESM optimization with TorchScript
+- [] create new bucket with input files
+- [] call Kubernetes Job Manager, include link to new bucket for application I/O
+- [] upload preference inference data to viz
+
+Inference
+- [x] test running ESMFold on GCP instance
+- [x] test running ESMFold with GPUs on GCP instance
+- [x] ~~ESM optimization using sharding~~ not worth it, running into issues loading the sharded model. I think the model is already sharded from HF since it's 15GB
+- [x] packaging ESM into a dockerfile
+- [] loading ESM dockerfile on GKE with reserved instances
+- [] saving files from ESM GKE
+- [] communicating files to frontend from GKE backend
+- [] ? emailing users of results
+
+- [] ESM masked probability task on jupyter notebook
+- [] add ESM masked probability to Docker image
+
+Backend (Kubernetes Job Manager)
+- [] read up on python kubernetes
+- [] spin up test Kubernetes job manager
